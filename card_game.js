a6e5afac8a700f7c1e90f828e4e0d0a3187dc409
fyzam <fyzam@fasy.at>
2020-06-10 12:53:05 +0200

    finshing added

--- card_game.js
+++ card_game.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+	var finished = 0;
 
 	for (let i = 0; i < 10; i++) {
 		$('#card_wrapper').append(`<div class="card" id="card_${i+1}">${i+1}</div>`);
@@ -7,16 +7,25 @@ $(document).ready(function () {
 	}
 
 
-	$(".card").draggable({revert: "invalid"});
+	$(".card").draggable({
+		revert: "invalid"
+	});
 
-	for (let i = 0; i < 10; i++){
-		$(`#slot_${i+1}`).droppable({accept: `#card_${i+1}`, drop: function(event, ui){
+	for (let i = 0; i < 10; i++) {
+		$(`#slot_${i+1}`).droppable({
+			accept: `#card_${i+1}`,
+			drop: function (event, ui) {
 				ui.draggable.remove();
-				($(this).addClass('green'));
+				$(this).addClass('green');
+				finished++;
+				console.log(finished);
+				if (finished == 10) {
+					$('#card_wrapper').remove();
+				}
 			}
-
 		});
 	};
+
+
+
 });
-
-
